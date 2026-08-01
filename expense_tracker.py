c9a4ea17bf39e7a8b7608d05b4925fc471c9c77e
chetna-code2009 <chetnarawar974@gmail.com>
2026-08-01 15:45:18 +0530

    add student expense tracker project

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,1 +1,32 @@
-print("expense tracker")
+expense={}
+
+def add_expense():
+     while True:
+          print("\n---^_^ADD EXPENSE^_^---")
+          category=input("ENTER EXPENSE CATEGORY:")
+          amount=float(input("ENTER AMOUNT{₹}:"))
+          expense={
+               "category":category,
+               "amount":amount
+          }
+          expenses.append(expense)
+          print("expense added successfully")
+          more=input("do you want add another expense?(yes/no):").lower()
+          if more != "yes":
+              break
+def view_expenses():
+     if not expenses:
+          print("\nNo expenses recorded.")
+          return
+     print("\n---Your Expenses---")
+     for index,expense in enumerate(expense,start=1):
+          print(f"{index}. Category: {expense['category']}, Amount: ₹{expense['amount']:.2f}") 
+
+def delete_expense():
+     if not expenses:
+          print("\nNo expenses to delete.")
+          return
+     view_expenses()
+     try:
+         number=int(input("Enter the number of the expense to delete:"))
+         
