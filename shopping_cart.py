9a2f9342fd11fbb70f83307e4235b3c93d7ee505
chetna-code2009 <chetnarawar974@gmail.com>
2026-08-12 20:51:08 +0530

    Add initial shopping cart functionality with product display

---
+++ shopping_cart.py
@@ -0,0 +1,14 @@
+products={101:{'name':'laptop','price':35000},102:{'name':'mobile','price':15000},103:{'name':'tablet','price':20000},104:{'name':'headphones','price':5000},105:{'name':'smartwatch','price':6000}}
+cart={}
+def display_products():
+    print("\n"+"="*50)
+    print("*^____^* PRODUCTS *^____^*")
+    print("\n"+"="*50)
+    print(f"{'ID':<8}{'Product':<20}{'Price':>10}")
+    print("-"*50)
+    for product_id, product in products.items():
+     print(f"{product_id:<8}"f"{product['name']:<20}"f"{product['price']:>9}")
+
+        
+        
+
